ad21d80daca4976cfdeb23709a970c545ee6a303
Mario Alvarez Picallo <malvarez@category.xyz>
2025-10-08 17:50:06 +0100

    Factor out opcodes

--- bin/evmrun.ml
+++ bin/evmrun.ml
@@ -1,10 +1,13 @@
 open Monad_lib
 open Chain.Ethereum
 
-let usage_str = "Usage: evmrun [--gas N] --bytecode_file bytecode_file --calldata calldata_file"
+let usage_str =
+  "Usage: evmrun [--gas N] [--trace] (--bytecode_file FILE | --bytecode HEX) (--calldata_file FILE | \
+   --bytecode HEX)"
 let bytecode_source = ref None
 let calldata_source = ref None
 let gas_limit = ref 100000L
+let trace = ref false
 
 let set_source_file r = Arg.String (fun s -> r := Some (`File s))
 let set_source_literal r = Arg.String (fun s -> r := Some (`Literal s))
@@ -16,7 +19,8 @@ let () =
       ; ("--calldata_file", set_source_file calldata_source, "Calldata file")
       ; ("--bytecode", set_source_literal bytecode_source, "Bytecode")
       ; ("--calldata", set_source_literal calldata_source, "Calldata")
-      ; ("--gas", String (fun s -> gas_limit := Int64.of_string s), "Gas limit (default: 100000)") ]
+      ; ("--gas", String (fun s -> gas_limit := Int64.of_string s), "Gas limit (default: 100000)")
+      ; ("--trace", Set trace, "Enable tracing") ]
       (fun extra_arg ->
         Format.printf "Unknown argument %s\n" extra_arg ;
         exit (-1) )
@@ -25,12 +29,13 @@ let () =
 let to_bytes str =
   let l = String.length str in
   assert (l mod 2 == 0) ;
-  String.init (l / 2) (fun i -> Char.chr (int_of_string (Printf.sprintf "0x%c%c" str.[i * 2] str.[(i * 2) + 1])))
+  String.init (l / 2) (fun i ->
+      Char.chr (int_of_string (Printf.sprintf "0x%c%c" str.[i * 2] str.[(i * 2) + 1])) )
 
 let read_source place =
   match !place with
-  | Some(`File file) -> In_channel.(with_open_bin file input_all)
-  | Some(`Literal lit) -> to_bytes lit
+  | Some (`File file) -> In_channel.(with_open_bin file input_all)
+  | Some (`Literal lit) -> to_bytes lit
   | None -> ""
 
 let bytecode = read_source bytecode_source
@@ -59,7 +64,7 @@ let msg =
       ; code_address = Address.zero
       ; code = bytecode } )
 
-let result, _state = Vm.call msg EvmcHost.State.empty
+let result, _state = Vm.call ~trace:!trace msg EvmcHost.State.empty
 
 let () =
   match result.status_code with Success -> Format.printf "Ok\n" | _ -> Format.printf "Execution failure\n"
