d0392ed92f3630ed42e8f57485491c55355b4d95
Mario Alvarez Picallo <malvarez@category.xyz>
2025-10-28 11:40:30 +0000

    Remove unused chain-level definitions

--- lib/chain/monad.ml
+++ lib/chain/monad.ml
@@ -1,16 +1,5 @@
+(** Definitions for Monad-specific types. *)
+
 module Revision = struct
-  type t = Zero | One | Two | Three | Four
-  module type SIG = sig
-    val rev : t
-  end
-end
-
-module Traits (Rev : Revision.SIG) = struct
-  let monad_rev = Rev.rev
-  let evm_rev =
-    if Revision.(monad_rev >= Four) then Ethereum.Revision.Prague else Ethereum.Revision.Cancun
-
-  let monad_pricing_version = if Revision.(monad_rev >= Four) then 1 else 0
-
-  let chain_id = 10143 (* testnet *)
+  type t = Zero | One | Two | Three | Four | Five | Next
 end
