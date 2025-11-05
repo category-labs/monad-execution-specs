7869724fc9048f993e93b0a1ff0c39d685f5dd27
Mario Alvarez Picallo <malvarez@category.xyz>
2025-11-05 16:27:40 +0000

    Ocamlformat

--- lib/uint64.ml
+++ lib/uint64.ml
@@ -6,7 +6,7 @@ let div = Stdlib.Int64.unsigned_div
 let rem = Stdlib.Int64.unsigned_rem
 let ( / ) = Stdlib.Int64.unsigned_div
 
-let (~$) = Stdlib.Int64.of_int
+let ( ~$ ) = Stdlib.Int64.of_int
 
 let compare = Stdlib.Int64.unsigned_compare
 
